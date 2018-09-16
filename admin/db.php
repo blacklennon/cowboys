7deadb95ad8e635d87bc0cbbbcc16f284bd36f45
Administrator <admin@example.com>
2018-09-16 16:39:12 +0200

    panel admin

---
+++ admin/db.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+* Configuration for database connection
+*/
+
+$host       = "cowboysfkj46.mysql.db";
+$username   = "cowboysfkj46";
+$password   = "KA9wDvb8rv6L";
+$dbname     = "cowboysfkj46";
+$dsn        = "mysql:host=$host;dbname=$dbname";
+$options    = array(
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            );
