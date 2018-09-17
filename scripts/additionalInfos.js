97f036d69a2150e6929dccda11211d90f22e9ca7
Administrator <admin@example.com>
2018-09-17 11:17:21 +0200

    Add tlux changes

--- scripts/additionalInfos.js
+++ scripts/additionalInfos.js
@@ -1,17 +1,20 @@
 function displayProjectInfo(page) {
   const projectInfos = document.getElementById('projectInfos');
   switch(page) {
+    case 0 :
+      projectInfos.innerHTML = 'Projet'
+      break;
     case 1 :
-      projectInfos.innerHTML = `Projet  - <b>Campaign Daphine SS18</b> - <a onClick="handleOpenInfo(1)">index</a>`;
+      projectInfos.innerHTML = `<i>Projet</i>  - <b>Campaign Daphine SS18</b> - <span onClick="handleOpenInfo(1)">index</span>`;
       break;
     case 2 :
-      projectInfos.innerHTML = `Projet  - <b>The creation behind AURA Mugler by its perfumer-creators</b> - <a onClick="handleOpenInfo(1)">index</a>`;
+      projectInfos.innerHTML = `<i>Projet</i>  - <b>The creation behind AURA Mugler by its perfumer-creators</b> - <span onClick="handleOpenInfo(1)">index</span>`;
       break;
     case 3 :
-      projectInfos.innerHTML = `Projet  - <b>Campaign Perrier-Jouët Blanc de Blanc</b> - <a onClick="handleOpenInfo(1)">index</a>`;
+      projectInfos.innerHTML = `<i>Projet</i>  - <b>Campaign Perrier-Jouët Belle Époque Vendanges</b> - <span onClick="handleOpenInfo(1)">index</span>`;
       break;
     case 4 :
-      projectInfos.innerHTML = `Projet  - <b>Campaign Perrier-Jouët Belle Époque Vendanges</b> - <a onClick="handleOpenInfo(1)">index</a>`;
+      projectInfos.innerHTML = `<i>Projet</i>  - <b>Campaign Perrier-Jouët Blanc de Blanc</b> - <span onClick="handleOpenInfo(1)">index</span>`;
       break;
   }
 }
--- scripts/slide.js
+++ scripts/slide.js
@@ -4,7 +4,6 @@ function homePageSlider() {
 
   if (blackLogo.className === 'black_logo_active') {
     blackLogo.className = 'black_logo_inactive';
-    projectInfos.style.opacity = "0";
   } else {
     blackLogo.className = 'black_logo_active'
     setTimeout(() => projectInfos.style.opacity = "1", 200);
