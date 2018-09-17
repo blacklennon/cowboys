5fdea42aa66dce573beeed4a14945eb7a524bf7d
Pierre CARLES <pcarles@e1r6p12.42.fr>
2018-09-17 13:17:25 +0200

    sdfsd

--- scripts/additionalInfos.js
+++ scripts/additionalInfos.js
@@ -5,16 +5,16 @@ function displayProjectInfo(page) {
       projectInfos.innerHTML = 'Projet'
       break;
     case 1 :
-      projectInfos.innerHTML = `<i>Projet</i>  - <b>Campaign Daphine SS18</b> - <span onClick="handleOpenInfo(1)">index</span>`;
+      projectInfos.innerHTML = `<i>Projet</i>  - <b>Campaign Daphine SS18</b> - <span onclick="handleOpenInfo(1)">index</span>`;
       break;
     case 2 :
-      projectInfos.innerHTML = `<i>Projet</i>  - <b>The creation behind AURA Mugler by its perfumer-creators</b> - <span onClick="handleOpenInfo(1)">index</span>`;
+      projectInfos.innerHTML = `<i>Projet</i>  - <b>The creation behind AURA Mugler by its perfumer-creators</b> - <span onclick="handleOpenInfo(1)">index</span>`;
       break;
     case 3 :
-      projectInfos.innerHTML = `<i>Projet</i>  - <b>Campaign Perrier-Jouët Belle Époque Vendanges</b> - <span onClick="handleOpenInfo(1)">index</span>`;
+      projectInfos.innerHTML = `<i>Projet</i>  - <b>Campaign Perrier-Jouët Belle Époque Vendanges</b> - <span onclick="handleOpenInfo(1)">index</span>`;
       break;
     case 4 :
-      projectInfos.innerHTML = `<i>Projet</i>  - <b>Campaign Perrier-Jouët Blanc de Blanc</b> - <span onClick="handleOpenInfo(1)">index</span>`;
+      projectInfos.innerHTML = `<i>Projet</i>  - <b>Campaign Perrier-Jouët Blanc de Blanc</b> - <span onclick="handleOpenInfo(1)">index</span>`;
       break;
   }
 }
