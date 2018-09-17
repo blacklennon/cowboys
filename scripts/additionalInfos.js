47871d29ea302a19d4783c02d41e252ef95a8551
Pierre CARLES <pcarles@e1r6p12.42.fr>
2018-09-17 17:29:39 +0200

    don't put hardcoded name in js

--- scripts/additionalInfos.js
+++ scripts/additionalInfos.js
@@ -1,20 +1,10 @@
 function displayProjectInfo(page) {
   const projectInfos = document.getElementById('projectInfos');
-  switch(page) {
-    case 0 :
-      projectInfos.innerHTML = 'Projet'
-      break;
-    case 1 :
-      projectInfos.innerHTML = `<i>Projet</i>  - <b>Campaign Daphine SS18</b> - <span onclick="handleOpenInfo(1)">index</span>`;
-      break;
-    case 2 :
-      projectInfos.innerHTML = `<i>Projet</i>  - <b>The creation behind AURA Mugler by its perfumer-creators</b> - <span onclick="handleOpenInfo(1)">index</span>`;
-      break;
-    case 3 :
-      projectInfos.innerHTML = `<i>Projet</i>  - <b>Campaign Perrier-Jouët Belle Époque Vendanges</b> - <span onclick="handleOpenInfo(1)">index</span>`;
-      break;
-    case 4 :
-      projectInfos.innerHTML = `<i>Projet</i>  - <b>Campaign Perrier-Jouët Blanc de Blanc</b> - <span onclick="handleOpenInfo(1)">index</span>`;
-      break;
+  let name = '';
+  if (page == 0) {
+    projectInfos.innerHTML = 'Projet';
+  } else {
+    name = document.querySelector(`#img_container${page} .projectDiv em`).innerText;
+    projectInfos.innerHTML = `<i>Projet</i>  - <b>${name}</b> - <span onclick="handleOpenInfo(1)">index</span>`;
   }
 }
